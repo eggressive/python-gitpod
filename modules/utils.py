91527678c70bb4222ec3f34cb7fb36609d2aa1de
Dimitar Dimitrov <mitko.69@gmail.com>
2024-02-29 13:00:14 +0000

    Add llama_chat function to utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -89,3 +89,56 @@ def llama(
     print(f"Tried {max_tries} times to make API call to get a valid response object")
     print("Returning provided response")
     return response
+
+def llama_chat(prompts, 
+               responses,
+               model="togethercomputer/llama-2-7b-chat", 
+               temperature=0.0, 
+               max_tokens=1024,
+               verbose=False,
+               url=url,
+               headers=headers,
+               base=2,
+               max_tries=3
+              ):
+
+    prompt = get_prompt_chat(prompts,responses)
+
+    # Allow multiple attempts to call the API incase of downtime.
+    # Return provided response to user after 3 failed attempts.    
+    wait_seconds = [base**i for i in range(max_tries)]
+
+    for num_tries in range(max_tries):
+        try:
+            response = llama(prompt=prompt,
+                             add_inst=False,
+                             model=model, 
+                             temperature=temperature, 
+                             max_tokens=max_tokens,
+                             verbose=verbose,
+                             url=url,
+                             headers=headers
+                            )
+            return response
+        except Exception as e:
+            if response.status_code != 500:
+                return response.json()
+
+            print(f"error message: {e}")
+            print(f"response object: {response}")
+            print(f"num_tries {num_tries}")
+            print(f"Waiting {wait_seconds[num_tries]} seconds before automatically trying again.")
+            time.sleep(wait_seconds[num_tries])
+ 
+    print(f"Tried {max_tries} times to make API call to get a valid response object")
+    print("Returning provided response")
+    return response
+
+
+def get_prompt_chat(prompts, responses):
+  prompt_chat = f"<s>[INST] {prompts[0]} [/INST]"
+  for n, response in enumerate(responses):
+    prompt = prompts[n + 1]
+    prompt_chat += f"\n{response}\n </s><s>[INST] \n{ prompt }\n [/INST]"
+
+  return prompt_chat
