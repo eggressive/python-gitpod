f51b9f54bfec6e75814a3f63152f2b75910718d8
Dimitar Dimitrov <mitko.69@gmail.com>
2024-03-04 11:51:44 +0000

    lesson 6; updated utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,22 +1,3 @@
-"""
-    Calls the Together API to generate a response based on the given prompt.
-    
-    Args:
-        prompt (str): The input prompt for the model.
-        add_inst (bool, optional): Whether to add an instruction tag to the prompt. Defaults to True.
-        model (str, optional): The name of the model to use. Defaults to "togethercomputer/llama-2-7b-chat".
-        temperature (float, optional): The temperature parameter for controlling the randomness of the output. Defaults to 0.0.
-        max_tokens (int, optional): The maximum number of tokens in the generated response. Defaults to 1024.
-        verbose (bool, optional): Whether to print additional information. Defaults to False.
-        url (str, optional): The URL of the Together API. Defaults to the value from the environment variable 'DLAI_TOGETHER_API_BASE'.
-        headers (dict, optional): The headers for the API request. Defaults to the authorization header with the value from the environment variable 'TOGETHER_API_KEY'.
-        base (int, optional): The base number of seconds to wait between API call attempts. Defaults to 2.
-        max_tries (int, optional): The maximum number of attempts to call the API. Defaults to 3.
-    
-    Returns:
-        str: The generated response from the model.
-"""
-
 import os
 from dotenv import load_dotenv
 import os
@@ -26,32 +7,83 @@ import requests
 import json
 import time
 
-# Initialize global variables
+# Initailize global variables
 _ = load_dotenv(find_dotenv())
 # warnings.filterwarnings('ignore')
 url = f"{os.getenv('DLAI_TOGETHER_API_BASE', 'https://api.together.xyz')}/inference"
 headers = {
-    "Authorization": f"Bearer {os.getenv('TOGETHER_API_KEY')}",
-    "Content-Type": "application/json",
-}
+        "Authorization": f"Bearer {os.getenv('TOGETHER_API_KEY')}",
+        "Content-Type": "application/json"
+    }
+# which allow for the largest number of
+# tokens for the input prompt: 
+# I think max_tokens set the maximum that can be output
+# but the sum of tokens from input prompt and response 
+# can not exceed 4097.
+def code_llama(prompt, 
+          model="togethercomputer/CodeLlama-7b-Instruct", 
+          temperature=0.0, 
+          max_tokens=1024,
+          verbose=False,
+          url=url,
+          headers=headers,
+          base=2,
+          max_tries=3):
+
+    if model.endswith("Instruct"):
+        prompt = f"[INST]{prompt}[/INST]"
+
+    if verbose:
+        print(f"Prompt:\n{prompt}\n")
+        print(f"model: {model}")
+
+    data = {
+            "model": model,
+            "prompt": prompt,
+            "temperature": temperature,
+            "max_tokens": max_tokens
+        }
+
+    # Allow multiple attempts to call the API incase of downtime.
+    # Return provided response to user after 3 failed attempts.
+    wait_seconds = [base**i for i in range(max_tries)]
+
+    for num_tries in range(max_tries):
+        try:
+            response = requests.post(url, headers=headers, json=data)
+            return response.json()['output']['choices'][0]['text']
+        except Exception as e:
+            if response.status_code != 500:
+                return response.json()
+
+            print(f"error message: {e}")
+            print(f"response object: {response}")
+            print(f"num_tries {num_tries}")
+            print(f"Waiting {wait_seconds[num_tries]} seconds before automatically trying again.")
+            time.sleep(wait_seconds[num_tries])
+ 
+    print(f"Tried {max_tries} times to make API call to get a valid response object")
+    print("Returning provided response")
+    return response
 
 
-import time
-
-
-def llama(
-    prompt,
-    add_inst=True,
-    model="togethercomputer/llama-2-7b-chat",
-    temperature=0.0,
-    max_tokens=1024,
-    verbose=False,
-    url=url,
-    headers=headers,
-    base=2,  # number of seconds to wait
-    max_tries=3,
-):
-
+# 20 is the minum new tokens, 
+# which allow for the largest number of
+# tokens for the input prompt: 4097 - 20 = 4077 
+# But max_tokens limits the number of output tokens
+# sum of input prompt tokens + max_tokens (response)
+# can't exceed 4097.
+def llama(prompt, 
+          add_inst=True, 
+          model="togethercomputer/llama-2-7b-chat", 
+          temperature=0.0, 
+          max_tokens=1024,
+          verbose=False,
+          url=url,
+          headers=headers,
+          base=2, # number of seconds to wait
+          max_tries=3):
+    
     if add_inst:
         prompt = f"[INST]{prompt}[/INST]"
 
@@ -60,20 +92,20 @@ def llama(
         print(f"model: {model}")
 
     data = {
-        "model": model,
-        "prompt": prompt,
-        "temperature": temperature,
-        "max_tokens": max_tokens,
-    }
+            "model": model,
+            "prompt": prompt,
+            "temperature": temperature,
+            "max_tokens": max_tokens
+        }
 
-    # Allow multiple attempts to call the API in case of downtime.
-    # Return provided response to user after 3 failed attempts.
+    # Allow multiple attempts to call the API incase of downtime.
+    # Return provided response to user after 3 failed attempts.    
     wait_seconds = [base**i for i in range(max_tries)]
 
     for num_tries in range(max_tries):
         try:
             response = requests.post(url, headers=headers, json=data)
-            return response.json()["output"]["choices"][0]["text"]
+            return response.json()['output']['choices'][0]['text']
         except Exception as e:
             if response.status_code != 500:
                 return response.json()
@@ -81,14 +113,13 @@ def llama(
             print(f"error message: {e}")
             print(f"response object: {response}")
             print(f"num_tries {num_tries}")
-            print(
-                f"Waiting {wait_seconds[num_tries]} seconds before automatically trying again."
-            )
+            print(f"Waiting {wait_seconds[num_tries]} seconds before automatically trying again.")
             time.sleep(wait_seconds[num_tries])
-
+ 
     print(f"Tried {max_tries} times to make API call to get a valid response object")
     print("Returning provided response")
     return response
+
 
 def llama_chat(prompts, 
                responses,
@@ -105,7 +136,7 @@ def llama_chat(prompts,
     prompt = get_prompt_chat(prompts,responses)
 
     # Allow multiple attempts to call the API incase of downtime.
-    # Return provided response to user after 3 failed attempts.    
+    # Return provided response to user after 3 failed attempts.
     wait_seconds = [base**i for i in range(max_tries)]
 
     for num_tries in range(max_tries):
